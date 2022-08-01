b3dbb04d8db2e9619ff62700e54f8ccac11ddd1b
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-01 18:10:44 -0300

    Login + Autenticaciones en Middleware

--- src/routes/Funciones.js
+++ src/routes/Funciones.js
@@ -20,8 +20,31 @@ const registrar = async (req, res) => {
     }
 }
 
+const login = async (req, res, next) => {
+    const { email, contraseña } = req.body
 
+    const existeUsuario = await User.findOne({ where : {email: email } })
+
+    if(!existeUsuario){
+        const error = new Error("No existe un usuario con ese Email")
+        return res.status(404).json({msg: error.message})
+    }
+
+    if(existeUsuario.contraseña !== contraseña) {
+        const error = new Error("Contraseña invalida")
+        return res.status(400).json({msg: error.message})
+    }
+
+    try {
+        res.cookie("usuarioId", existeUsuario.id);
+        res.json("Login exitoso")
+        // res.redirect("/feed");
+    } catch (error) {
+        next(error)
+    }
+}
 
 module.exports = {
     registrar,
+    login,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,28 @@ const { Users } = require ("../db")
 const { Songs } = require ("../db")
 const axios = require("axios");
 const { API_KEY } = process.env;
-const { registrar } = require("./Funciones.js")
+const { registrar, login } = require("./Funciones.js")
 
 const router = Router();
 
-router.post("/", registrar)
+const isAuthenticated = (req, res, next) => {
+    if (!req.cookies.userId) {
+      return res.redirect("/login")
+    };
+    next();
+  }
+  
+  const isNotAuthenticated = (req, res, next) => {
+    if (req.cookies.userId) {
+      return res.redirect("/home")
+    };
+    next();
+  
+  }
+
+router.post("/register", registrar, isNotAuthenticated)
+router.post("/login", login, isNotAuthenticated)
+// router.post("/home", isAuthenticated)
 
 
 module.exports = router;
