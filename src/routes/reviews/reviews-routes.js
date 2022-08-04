6e90a16a7d31bedfadb3b2da58199044d72e3e6a
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-04 15:09:39 -0300

    route conflict fix

--- src/routes/reviews/reviews-routes.js
+++ src/routes/reviews/reviews-routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { crear } = require("./FuncionesReviews.js");
+const { crear } = require("./reviews-functions.js");
 const router = Router();
 
 router.post("/create", crear)
