cc72bf163e13422c10bf17856977c2363dc732b7
Julian <julianlechuga12@gmail.com>
2022-08-06 20:38:31 -0300

    minor fix

--- src/routes/users/usersRoutes.js
+++ src/routes/users/usersRoutes.js
@@ -1,19 +1,7 @@
-<<<<<<< HEAD:src/routes/users/usersRoutes.js
-const express = require("express");
-const { registrar, confirmar, autenticar, perfil, sendEmailContact, olvidePassword, comprobarToken, nuevaPassword } = require("./FuncionesUsers.js");
-const { checkAutenticacion } = require("../../middelwear/authMiddelwear");
-
-const usersRoutes = express.Router();
-=======
 const express = require("express")
 const { registrar, confirmar, autenticar, perfil, sendEmailContact, olvidePassword, comprobarToken, nuevaPassword, crearPagoMELI, baseApremium } = require("./FuncionesUsers.js");
-const { checkAutenticacion } = require("../middelwear/authMiddelwear");
-
-
+const { checkAutenticacion } = require("../../middleware/authMiddleware");
 const usersRoutes = express.Router()
->>>>>>> d51537b9c0d6f513e9b38d851be7d20a12434fbb:src/routes/usersRoutes.js
-
-
 
 usersRoutes.post("/create_preference" , checkAutenticacion, crearPagoMELI ); // tarjetas adentro de la funcion crearPagoMELI
 
