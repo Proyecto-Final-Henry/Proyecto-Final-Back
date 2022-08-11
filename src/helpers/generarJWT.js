e04191a367d3ef3f708d17015472627a3eea6611
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-11 12:25:27 -0300

    localhost removed

--- src/helpers/generarJWT.js
+++ src/helpers/generarJWT.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken")
 const generarJWT = (email) => {
     return jwt.sign({ email }, process.env.JWT_SECRET, {
        expiresIn: "30d"
-    })
-}
-
-module.exports = { generarJWT }
+    });
+};
+module.exports = { generarJWT };
