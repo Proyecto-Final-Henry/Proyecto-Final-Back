39766dc2ed4b46136012173718a7c6ce96f83942
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-17 12:23:36 -0300

    traer followers

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,14 +7,10 @@ const checkAutenticacion = async (req,res,next) => {
         try {
             token = req.headers.authorization.split(" ")[1]
             const decodificarToken = jwt.verify(token, process.env.JWT_SECRET)
-            
-            req.usuario = await User.findOne({where:{email: decodificarToken.email}},{include: [{
+            req.usuario = await User.findOne({where:{email: decodificarToken.email} ,include: [{
                 model: Review
-            }, "followers", "following"]})
-            
-            
-            return next()
-
+            }, "followers", "following"]});
+            return next();
         } catch (error) {
             const e = new Error("Token invalido")
             return res.status(403).json({msg: e.message})
@@ -24,7 +20,7 @@ const checkAutenticacion = async (req,res,next) => {
     if(!token){
         const error = new Error ("Token no valido o inexistente")
         res.status(403).json({msg: error.message})
-    }
+    };
     next();
 };
 
