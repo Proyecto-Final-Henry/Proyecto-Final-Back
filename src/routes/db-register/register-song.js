72278e35243bbbbd2ce72e3ff7f1391d101b633c
lal-fac <luis.alfonso.ep@gmail.com>
2022-08-19 18:07:59 -0500

    feat(eliminación de usuario definitiva para admin)

--- src/routes/db-register/register-song.js
+++ src/routes/db-register/register-song.js
@@ -1,4 +1,6 @@
 const { Song } = require("./../../db.js");
+const registerArtist = require("./register-artist");
+const registerAlbum = require("./register-album");
 const axios = require("axios");
 
 async function registerSong(name, apiId) {
@@ -8,6 +10,10 @@ async function registerSong(name, apiId) {
       preview: result.data.preview,
       duration: result.data.duration,
       img: result.data.album.cover_big,
+      artist: result.data.artist.name,
+      artistId: result.data.artist.id,
+      album: result.data.album.title,
+      albumId: result.data.album.id,
     };
     const [song, created] = await Song.findOrCreate({
       where: {
@@ -18,6 +24,10 @@ async function registerSong(name, apiId) {
         img: apiSong.img,
       },
     });
+    const { album } = await registerAlbum(apiSong.album, apiSong.albumId);
+    await album.addSong(song.id);
+    const { artist } = await registerArtist(apiSong.artist, apiSong.artistId);
+    await artist.addSong(song.id);
     return { song, created };
   } catch (err) {
     console.log(err);
